ce72117890a372213e7afdd25630a4bafbee07f8
Xylopyrographer <xylopyrographer@gmail.com>
2026-02-25 16:17:10 -0700

    Fix loadRecentCities: use isKey() probe to avoid NOT_FOUND log errors when list is empty

--- src/data/recent.cpp
+++ src/data/recent.cpp
@@ -11,12 +11,11 @@ void loadRecentCities() {
     prefs.begin( "sys", false );
     for ( int i = 0; i < MAX_RECENT_CITIES; i++ ) {
         String prefix = "recent" + String( i );
-        String city = prefs.getString( ( prefix + "c" ).c_str(), "" );
-        if ( city.length() == 0 ) {
+        if ( !prefs.isKey( ( prefix + "c" ).c_str() ) ) {
             break;
         }
-        recentCities[ i ].city = city;
-        recentCities[ i ].country = prefs.getString( ( prefix + "co" ).c_str(), "" );
+        recentCities[ i ].city    = prefs.getString( ( prefix + "c" ).c_str(), "" );
+        recentCities[ i ].country  = prefs.getString( ( prefix + "co" ).c_str(), "" );
         recentCities[ i ].timezone = prefs.getString( ( prefix + "tz" ).c_str(), "" );
         recentCities[ i ].gmtOffset = prefs.getInt( ( prefix + "go" ).c_str(), 3600 );
         recentCities[ i ].dstOffset = prefs.getInt( ( prefix + "do" ).c_str(), 3600 );
